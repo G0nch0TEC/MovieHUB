b69f34ccfb3e56fc0c1071bab3c27cd45b6ac243
G0nch0TEC <benzaquenrios@gmail.com>
2026-07-12 11:46:44 -0500

    Creando las carpetas necesarias

---
+++ MovieHub-BackEnd/src/main/java/com/proyect/MovieHub_BackEnd/MovieHubBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.proyect.MovieHub_BackEnd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieHubBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieHubBackEndApplication.class, args);
+	}
+
+}
---
+++ MovieHub-BackEnd/src/test/java/com/proyect/MovieHub_BackEnd/MovieHubBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.proyect.MovieHub_BackEnd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieHubBackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
